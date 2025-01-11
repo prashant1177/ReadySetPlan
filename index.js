ee72c913f7109a537b8f8382ea607e744c1c1635
prashant1177 <prashantjpatil49@gmail.com>
2025-01-11 22:10:39 +0530

    Basic set up

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-const express = require('express');
-const app = express();
+// server.js
+var express = require("express"),
+  engine = require("ejs-mate"),
+  app = express();
+const path = require("path");
+const mongoose = require("mongoose");
 
-app.get('/', function (req, res) {
-  res.send('Hello World');
+mongoose.connect('mongodb://localhost:27017/eventPlanning');
+
+const bodyParser = require("body-parser");
+app.engine("ejs", engine);
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());// For parsing application/x-www-form-urlencoded
+app.use(express.static(path.join(__dirname, 'public'))); // Static files
+
+app.use('/', (req, res)=>{
+  console.log("Index.ejs is loading...")
+  res.render("index.ejs");
 });
 
-app.listen(3000, ()=> {
-    console.log("Server is running on 3000");
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
 });
